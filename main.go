a08e18a4d7086c82bfda34ee2c203bfa9a88e5b3
NASA12345 <34130571+NASA12345@users.noreply.github.com>
2020-01-12 22:55:44 +0530

    GCI-<Nayan>:Done

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
-package main
 
-import ("github.com/gin-gonic/gin"
-        "net/http"
-)
-func main() {
-	router := gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
-	})
-  router.GET("/greet/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello " + name + " !")
-	})
-	router.Run(":3000")
-}
